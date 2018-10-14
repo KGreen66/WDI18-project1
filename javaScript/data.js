cb24315029f4f405d957c141c37cdce4840030fd
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-10-14 19:06:44 -0400

    mvp done. 1 round functioning and basic styled

--- javaScript/data.js
+++ javaScript/data.js
@@ -2,31 +2,31 @@ const scienceQs = [
     {
         value: 100,
         question: "What color of light is produced when you combine light from each spectrum?",
-        answer: ['White', 'Black', 'Rainbow'],
+        answers: ['White', 'Black', 'Rainbow'],
         key: 0
     },
     {
         value: 200,
         question: "What is the hottest planet in our solar system?",
-        answer: ['Venus', 'Jupiter', 'Mars'],
+        answers: ['Venus', 'Jupiter', 'Mars'],
         key: 0
     },
     {
         value: 300,
         question: "What law states that all energy is constant; it can neither be created or destroyed, but instead can only be transferred or transformed?",
-        answer: ['Law of Thermodynamics', 'Law of Conservation of Energy', 'Law of Order'],
+        answers: ['Law of Thermodynamics', 'Law of Conservation of Energy', 'Law of Order'],
         key: 1
     },
     {
         value: 400,
         question: "What is considered the strongest magnetic force in the universe?",
-        answer: ['Black Hole', 'Nuetron Star', 'Polarity'],
+        answers: ['Black Hole', 'Nuetron Star', 'Polarity'],
         key: 1
     },
     {
         value: 500,
         question: "What is the only metal that is a liquid at room temperature",
-        answer: ['Cesium', 'Plutonium', 'Mercury'],
+        answers: ['Cesium', 'Plutonium', 'Mercury'],
         key: 2
     }
 ]
@@ -35,31 +35,31 @@ const gamingQs = [
     {
         value: 100,
         question: "What color is Mario's hat?",
-        answer: ['Peach', 'Green', 'Red'],
+        answers: ['Peach', 'Green', 'Red'],
         key: 2
     },
     {
         value: 200,
         question: "What country does the second Assassin's Creed take place?",
-        answer: ['Turkey', 'Italy', 'America'],
+        answers: ['Turkey', 'Italy', 'America'],
         key: 1
     },
     {
         value: 300,
         question: "What was the first Fallout game that took the franchise from it's turn-based RPG roots, to a FPS open-world action RPG?",
-        answer: ['Fallout 2', 'Fallout 76', 'Fallout 3'],
+        answers: ['Fallout 2', 'Fallout 76', 'Fallout 3'],
         key: 2
     },
     {
         value: 400,
         question: "Who is the main character in Red Dead Redemption?",
-        answer: ['Jack Marston', 'John Marston', 'Joe Marston'],
+        answers: ['Jack Marston', 'John Marston', 'Joe Marston'],
         key: 1
     },
     {
         value: 500,
         question: "What was the original battle-car in Rocket League?",
-        answer: ['Dominus', 'Octane', 'Breakout'],
+        answers: ['Dominus', 'Octane', 'Breakout'],
         key: 1
     }
 ]
@@ -68,31 +68,31 @@ const capitalsQs = [
     {
         value: 100,
         question: "What is the capital of the state of Georgia?",
-        answer: ['Atlanta', 'Athens', 'Peachtree City'],
+        answers: ['Atlanta', 'Athens', 'Peachtree City'],
         key: 0
     },
     {
         value: 200,
         question: "What is the capital of the state of Colorado?",
-        answer: ['Grand Junction', 'Boulder', 'Denver'],
+        answers: ['Grand Junction', 'Boulder', 'Denver'],
         key: 2
     },
     {
         value: 300,
         question: "What is the capital of Arkansas?",
-        answer: ['Little Rock', 'Big Rock', 'Rock'], 
+        answers: ['Little Rock', 'Big Rock', 'Rock'], 
         key: 0
     },
     {
         value: 400,
         question: "What is the capital of Texas?",
-        answer: ['Houston', 'Austin', 'Dallas'],
+        answers: ['Houston', 'Austin', 'Dallas'],
         key: 1
     },
     {
         value: 500,
         question: "What is the capital of North Dakota?",
-        answer: ['Bismarck', 'Raleigh', 'Pierre'],
+        answers: ['Bismarck', 'Raleigh', 'Pierre'],
         key: 0
     }
 ]
@@ -101,31 +101,31 @@ const historyQs = [
     {
         value: 100,
         question: `What year did the United States declare their indepence from Britain with the Declaration of Indepence?`,
-        answer: ['1766', '1776', '1876'],
+        answers: ['1766', '1776', '1876'],
         key: 1
     },
     {
         value: 200,
         question: "What was the first Eurpopean settlement in America?",
-        answer: ['Roanoke', 'Jamestown', 'St. Augustine'],
+        answers: ['Roanoke', 'Jamestown', 'St. Augustine'],
         key: 2
     },
     {
         value: 300,
         question: 'Who wrote the epics "The Illiad" and "The Odyssey"?',
-        answer: ['Aristotle', 'Homer', 'Agamemnon'],
+        answers: ['Aristotle', 'Homer', 'Agamemnon'],
         key: 1
     },
     {
         value: 400,
         question: `At the height of their reign, what empire was the largest in history?`,
-        answer: ['British Empire', 'Roman Empire', 'Alexander the Great'],
+        answers: ['British Empire', 'Roman Empire', 'Alexander the Great'],
         key: 0
     },
     {
         value: 500,
         question: `What civilization constructed the first known water pipes and indoor plumbing?`,
-        answer: ['Romans', 'Minoans', 'Babylonians'],
+        answers: ['Romans', 'Minoans', 'Babylonians'],
         key: 1
     }
 ]
@@ -134,98 +134,98 @@ const sportsQs = [
     {
         value: 100,
         question: "Who was the shortest player to ever play in the NBA?",
-        answer: ['Tyrone Bogues', 'Alan Iverson', 'Michael Jordan'],
+        answers: ['Tyrone Bogues', 'Alan Iverson', 'Michael Jordan'],
         key: 0
     },
     {
         value: 200,
         question: "What male tennis player has won the most Grand Slam titles?",
-        answer: ['John McEnroe', 'Roger Federer', 'Ken Rosewall'],
+        answers: ['John McEnroe', 'Roger Federer', 'Ken Rosewall'],
         key: 1
     },
     {
         value: 300,
         question: `Who is the only professional athlete to play in a Super Bowl and a World Series?`,
-        answer: ['Bo Jackson', 'Deion Sanders', 'Jerry Rice'],
+        answers: ['Bo Jackson', 'Deion Sanders', 'Jerry Rice'],
         key: 1
     },
     {
         value: 400,
         question: 'Where did the sport of "curling" originate?',
-        answer: ['Ireland', 'Scotland', 'Canada'],
+        answers: ['Ireland', 'Scotland', 'Canada'],
         key: 1
     },
     {
         value: 500,
         question: 'In what year did the first American football title game, the Super Bowl, take place?',
-        answer: ['1889', '1949', '1967'],
+        answers: ['1889', '1949', '1967'],
         key: 2
     }
 ]
 
 // Round Two
+// const  = [
+//     {
+//         value: 200,
+//         question: 
+//         answers: 
+//         key: 
+//     },
+//     {
+//         value: 400,
+//         question: 
+//         answers: 
+//         key: 
+//     },
+//     {
+//         value: 600,
+//         question: 
+//         answers: 
+//         key: 
+//     },
+//     {
+//         value: 800,
+//         question: 
+//         answers: 
+//         key: 
+//     },
+//     {
+//         value: 1000,
+//         question: 
+//         answers: 
+//         key: 
+//     }
+// ]
+
 // const = [
 //     {
 //         value: 200,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 400,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 600,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 800,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 1000,
 //         question: 
-//         answer: 
-//         key: 
-//     }
-// ]
-
-// const = [
-//     {
-//         value: 200,
-//         question: 
-//         answer: 
-//         key: 
-//     },
-//     {
-//         value: 400,
-//         question: 
-//         answer: 
-//         key: 
-//     },
-//     {
-//         value: 600,
-//         question: 
-//         answer: 
-//         key: 
-//     },
-//     {
-//         value: 800,
-//         question: 
-//         answer: 
-//         key: 
-//     },
-//     {
-//         value: 1000,
-//         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     }
 // ]
@@ -234,31 +234,31 @@ const sportsQs = [
 //     {
 //         value: 200,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 400,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 600,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 800,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 1000,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     }
 // ]
@@ -267,31 +267,31 @@ const sportsQs = [
 //     {
 //         value: 200,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 400,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 600,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 800,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 1000,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     }
 // ]
@@ -300,31 +300,31 @@ const sportsQs = [
 //     {
 //         value: 200,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 400,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 600,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 800,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     },
 //     {
 //         value: 1000,
 //         question: 
-//         answer: 
+//         answers: 
 //         key: 
 //     }
 // ]
--- javaScript/script.js
+++ javaScript/script.js
@@ -1,19 +1,44 @@
-const submit = (answerIndex, key) => {
-    console.log(`You are ${answerIndex === key ? 'correct!' : 'an idiot'}`);
-    panel.classList.toggle('open')
+const panel = document.querySelector('#panel');
+const points = document.querySelector('#playerPoints')
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+const dailyCat = getRandomInt(5)
+const dailyQuest = getRandomInt(5)
+
+const submit = (answerIndex, key, value) => {
+    let newPoints = +points.innerHTML;
+    //if run out of time, pass -number
+    //display ran out of time
+    if (answerIndex === key) {
+        panel.appendChild(document.createTextNode(`You are correct!`));
+        newPoints += value;
+    } else {
+        panel.appendChild(document.createTextNode(`You are incorrect!`));
+    }
+    setTimeout(() => panel.classList.toggle('open'), 2000);
+    points.innerHTML = newPoints
 }
 
 const togglePanel = q => {
-    const panel = document.querySelector('#panel');
     panel.innerHTML = '';
     panel.appendChild(document.createTextNode(q.question));
+    const div = document.createElement('div')
+    div.classList.add('buttonContainer')
+    panel.appendChild(div)
     q.answers.forEach((a, index) => {
         const button = document.createElement('button');
         button.appendChild(document.createTextNode(a));
-        button.addEventListener('click', () => submit(index, q.key));
-        panel.appendChild(button);
+        button.addEventListener('click', () => submit(index, q.key, q.value));
+        div.appendChild(button)
     });
     panel.classList.toggle('open')
+    //create div (30)
+    const timer = document.createElement('div')
+    //setinterval(1000)
+    //create a function that if i !== 0 to subtract 1
+    //else if i === 0, display out of time, clearInterval, close window function
+    
 };
 
 const populateColumn = (columnId, questions) => {
@@ -21,7 +46,10 @@ const populateColumn = (columnId, questions) => {
         const q = document.createElement('button');
         q.appendChild(document.createTextNode(question.value));
         q.classList.add('card');
-        q.addEventListener('click', () => togglePanel(question));
+        q.addEventListener('click', () => {
+            togglePanel(question);
+            q.classList.add('answered');
+        });
         document.querySelector(`#${columnId}`).appendChild(q);
     });
 }
@@ -31,3 +59,4 @@ populateColumn('gaming', gamingQs);
 populateColumn('cities', capitalsQs);
 populateColumn('history', historyQs);
 populateColumn('sports', sportsQs);
+
