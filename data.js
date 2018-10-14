b76208f82ef6e33035575ce7f5e9372f841d2bb4
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-10-14 14:26:34 -0400

    first round questions and html restructured SYBBTY

---
+++ data.js
@@ -0,0 +1,330 @@
+const scienceQs = [
+    {
+        value: 100,
+        question: "What color of light is produced when you combine light from each spectrum?",
+        answer: ['White', 'Black', 'Rainbow'],
+        key: 0
+    },
+    {
+        value: 200,
+        question: "What is the hottest planet in our solar system?",
+        answer: ['Venus', 'Jupiter', 'Mars'],
+        key: 0
+    },
+    {
+        value: 300,
+        question: "What law states that all energy is constant; it can neither be created or destroyed, but instead can only be transferred or transformed?",
+        answer: ['Law of Thermodynamics', 'Law of Conservation of Energy', 'Law of Order'],
+        key: 1
+    },
+    {
+        value: 400,
+        question: "What is considered the strongest magnetic force in the universe?",
+        answer: ['Black Hole', 'Nuetron Star', 'Polarity'],
+        key: 1
+    },
+    {
+        value: 500,
+        question: "What is the only metal that is a liquid at room temperature",
+        answer: ['Cesium', 'Plutonium', 'Mercury'],
+        key: 2
+    }
+]
+
+const gamingQs = [
+    {
+        value: 100,
+        question: "What color is Mario's hat?",
+        answer: ['Peach', 'Green', 'Red'],
+        key: 2
+    },
+    {
+        value: 200,
+        question: "What country does the second Assassin's Creed take place?",
+        answer: ['Turkey', 'Italy', 'America'],
+        key: 1
+    },
+    {
+        value: 300,
+        question: "What was the first Fallout game that took the franchise from it's turn-based RPG roots, to a FPS open-world action RPG?",
+        answer: ['Fallout 2', 'Fallout 76', 'Fallout 3'],
+        key: 2
+    },
+    {
+        value: 400,
+        question: "Who is the main character in Red Dead Redemption?",
+        answer: ['Jack Marston', 'John Marston', 'Joe Marston'],
+        key: 1
+    },
+    {
+        value: 500,
+        question: "What was the original battle-car in Rocket League?",
+        answer: ['Dominus', 'Octane', 'Breakout'],
+        key: 1
+    }
+]
+
+const capitalsQs = [
+    {
+        value: 100,
+        question: "What is the capital of the state of Georgia?",
+        answer: ['Atlanta', 'Athens', 'Peachtree City'],
+        key: 0
+    },
+    {
+        value: 200,
+        question: "What is the capital of the state of Colorado?",
+        answer: ['Grand Junction', 'Boulder', 'Denver'],
+        key: 2
+    },
+    {
+        value: 300,
+        question: "What is the capital of Arkansas?",
+        answer: ['Little Rock', 'Big Rock', 'Rock'], 
+        key: 0
+    },
+    {
+        value: 400,
+        question: "What is the capital of Texas?",
+        answer: ['Houston', 'Austin', 'Dallas'],
+        key: 1
+    },
+    {
+        value: 500,
+        question: "What is the capital of North Dakota?",
+        answer: ['Bismarck', 'Raleigh', 'Pierre'],
+        key: 0
+    }
+]
+
+const historyQs = [
+    {
+        value: 100,
+        question: `What year did the United States declare their indepence from Britain with the Declaration of Indepence?`,
+        answer: ['1766', '1776', '1876'],
+        key: 1
+    },
+    {
+        value: 200,
+        question: "What was the first Eurpopean settlement in America?",
+        answer: ['Roanoke', 'Jamestown', 'St. Augustine'],
+        key: 2
+    },
+    {
+        value: 300,
+        question: 'Who wrote the epics "The Illiad" and "The Odyssey"?',
+        answer: ['Aristotle', 'Homer', 'Agamemnon'],
+        key: 1
+    },
+    {
+        value: 400,
+        question: `At the height of their reign, what empire was the largest in history?`,
+        answer: ['British Empire', 'Roman Empire', 'Alexander the Great'],
+        key: 0
+    },
+    {
+        value: 500,
+        question: `What civilization constructed the first known water pipes and indoor plumbing?`,
+        answer: ['Romans', 'Minoans', 'Babylonians'],
+        key: 1
+    }
+]
+
+const sportsQs = [
+    {
+        value: 100,
+        question: "Who was the shortest player to ever play in the NBA?",
+        answer: ['Tyrone Bogues', 'Alan Iverson', 'Michael Jordan'],
+        key: 0
+    },
+    {
+        value: 200,
+        question: "What male tennis player has won the most Grand Slam titles?",
+        answer: ['John McEnroe', 'Roger Federer', 'Ken Rosewall'],
+        key: 1
+    },
+    {
+        value: 300,
+        question: `Who is the only professional athlete to play in a Super Bowl and a World Series?`,
+        answer: ['Bo Jackson', 'Deion Sanders', 'Jerry Rice'],
+        key: 1
+    },
+    {
+        value: 400,
+        question: 'Where did the sport of "curling" originate?',
+        answer: ['Ireland', 'Scotland', 'Canada'],
+        key: 1
+    },
+    {
+        value: 500,
+        question: 'In what year did the first American football title game, the Super Bowl, take place?',
+        answer: ['1889', '1949', '1967'],
+        key: 2
+    }
+]
+
+// Round Two
+const = [
+    {
+        value: 200,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 400,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 600,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 800,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 1000,
+        question: 
+        answer: 
+        key: 
+    }
+]
+
+const = [
+    {
+        value: 200,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 400,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 600,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 800,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 1000,
+        question: 
+        answer: 
+        key: 
+    }
+]
+
+const  = [
+    {
+        value: 200,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 400,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 600,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 800,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 1000,
+        question: 
+        answer: 
+        key: 
+    }
+]
+
+const  = [
+    {
+        value: 200,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 400,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 600,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 800,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 1000,
+        question: 
+        answer: 
+        key: 
+    }
+]
+
+const  = [
+    {
+        value: 200,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 400,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 600,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 800,
+        question: 
+        answer: 
+        key: 
+    },
+    {
+        value: 1000,
+        question: 
+        answer: 
+        key: 
+    }
+]
